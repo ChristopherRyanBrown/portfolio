17250d722841bf7cd30b0fb9c0971c74f8b8c7d6
Christopher Brown <christopherbrown@Christophers-MacBook-Pro-2.local>
2023-12-22 11:49:49 -0800

    feat: added svgs for chess pieces

--- src/chess/components/chess-piece/index.tsx
+++ src/chess/components/chess-piece/index.tsx
@@ -2,26 +2,40 @@ import { Box } from "@mui/material";
 import { useMemo } from "react";
 import { PieceType } from "../../enums/piece-type";
 import { Props } from "./props";
+import BlackBishop from "../../assets/BlackBishop.svg";
+import BlackKing from "../../assets/BlackKing.svg";
+import BlackKnight from "../../assets/BlackKnight.svg";
+import BlackPawn from "../../assets/BlackPawn.svg";
+import BlackQueen from "../../assets/BlackQueen.svg";
+import BlackRook from "../../assets/BlackRook.svg";
+import WhiteBishop from "../../assets/WhiteBishop.svg";
+import WhiteKing from "../../assets/WhiteKing.svg";
+import WhiteKnight from "../../assets/WhiteKnight.svg";
+import WhitePawn from "../../assets/WhitePawn.svg";
+import WhiteQueen from "../../assets/WhiteQueen.svg";
+import WhiteRook from "../../assets/WhiteRook.svg";
+import { Color } from "../../enums/color";
 
 export function ChessPiece({ piece }: Props) {
-  const { type } = piece;
+  const { color, type } = piece;
 
   const image = useMemo(() => {
+    const isBlack = color === Color.BLACK;
     switch (type) {
       case PieceType.BISHOP:
-        return "B";
+        return isBlack ? BlackBishop : WhiteBishop;
       case PieceType.KING:
-        return "K";
+        return isBlack ? BlackKing : WhiteKing;
       case PieceType.KNIGHT:
-        return "Kn";
+        return isBlack ? BlackKnight : WhiteKnight;
       case PieceType.PAWN:
-        return "P";
+        return isBlack ? BlackPawn : WhitePawn;
       case PieceType.QUEEN:
-        return "Q";
+        return isBlack ? BlackQueen : WhiteQueen;
       case PieceType.ROOK:
-        return "R";
+        return isBlack ? BlackRook : WhiteRook;
     }
   }, [type]);
 
-  return <Box>{image}</Box>;
+  return <img src={image} />;
 }
