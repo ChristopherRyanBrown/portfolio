7e06ddaff520be716a57311842c761f1ad6e05f4
Christopher Brown <christopherbrown@Christophers-MacBook-Pro-2.local>
2023-12-28 18:41:12 -0800

    fix: improved heuristic

--- src/chess/classes/heuristic/get-mapping.ts
+++ src/chess/classes/heuristic/get-mapping.ts
@@ -4,16 +4,16 @@ import { Piece } from "../piece";
 export function getMapping({ type }: Piece) {
   switch (type) {
     case PieceType.BISHOP:
-      return 7000;
+      return 3000;
     case PieceType.KING:
-      return 10000;
+      return 200000;
     case PieceType.KNIGHT:
-      return 8000;
+      return 3000;
     case PieceType.PAWN:
-      return 2000;
+      return 1000;
     case PieceType.QUEEN:
       return 9000;
     case PieceType.ROOK:
-      return 7000;
+      return 5000;
   }
 }
